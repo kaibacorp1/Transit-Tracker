3c9f718f4a47cc9d0a31574a2dac478ae645d9cd
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-05-29 21:46:23 +1000

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -158,6 +158,7 @@ function success(position) {
 
 function error(err) {
   alert(`Could not get your location. Reason: ${err.message}`);
+  startAutoRefresh();   // kick off your regular scan loop anyway
 }
 
 function updateLocationUI(lat, lon, elev) {
