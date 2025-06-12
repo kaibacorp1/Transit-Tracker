1707f38e43477df4a59b5aa7c678c5fb5fc6c609
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-06-12 20:56:56 +1000

    Update transitUtils.js

--- utils/transitUtils.js
+++ utils/transitUtils.js
@@ -93,7 +93,7 @@ export function detectTransits({
           callsign,
           azimuth: azimuth.toFixed(1),
           altitudeAngle: elevationAngle.toFixed(1),
-          track:           f.track,   // ← include the track field
+          track,          
           distance: distance.toFixed(1),
           selectedBody,
           predictionSeconds: predictSeconds
