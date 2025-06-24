9612d6263889d1f3104fe72a1fb7b2afcf2eaeee
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-06-25 00:28:32 +1000

    Update detect-transit.js

--- api/detect-transit.js
+++ api/detect-transit.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         userLat,
         userLon: normalizedLon,
         marginDegrees: margin,
-        searchRadiusKm: 100,
+        searchRadiusKm: 1000,
         predictSeconds
       });
       return res.status(200).json({ matches });
