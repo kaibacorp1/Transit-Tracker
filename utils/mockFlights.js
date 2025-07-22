4a68883796a77d555d27eacf363bd2f3184051da
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-07-22 16:53:28 +1000

    Add files via upload

---
+++ utils/mockFlights.js
@@ -0,0 +1,20 @@
+export const sampleFlights = [
+  {
+    callsign: "TEST123",
+    latitude: -33.85,
+    longitude: 151.1,
+    altitude: 5000,
+    heading: 190,
+    speed: 220,
+    verticalSpeed: -200
+  },
+  {
+    callsign: "EXAMPLE456",
+    latitude: -33.88,
+    longitude: 151.05,
+    altitude: 8000,
+    heading: 175,
+    speed: 250,
+    verticalSpeed: 0
+  }
+];
