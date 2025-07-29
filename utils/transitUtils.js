b4a39aadff215448cd88424a6880d5fd7280f8f7
kaibacorp1 <sandu.godakumbura@gmail.com>
2025-07-29 20:23:48 +1000

    Update transitUtils.js

--- utils/transitUtils.js
+++ utils/transitUtils.js
@@ -164,7 +164,7 @@ if (sep > maxAllowedSep) {
 };
 
   // ✅ Sweep time window every 1 second up to predictSeconds
-  for (let t = 0; t <= predictSeconds; t += 2) {
+  for (let t = 0; t <= predictSeconds; t += 1) {
     checkTransitsAt(t);
   }
 
